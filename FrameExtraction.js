4bccefbce85d4c0076fade888777eec3565083a2
MoonStartMan <974556594@qq.com>
2023-10-07 14:13:07 +0800

    fix: 修改逻辑

--- FrameExtraction.js
+++ FrameExtraction.js
@@ -9,36 +9,40 @@ function selectVideo() {
 
         // 检查文件类型是否为MP4
         if (file.type === 'video/mp4') {
-
             const fileURL = URL.createObjectURL(file);
             videoPlayer.src = fileURL;
+            videoPlayer.addEventListener('loadedmetadata', function() {
+                extractFrames(videoPlayer);
+                URL.revokeObjectURL(fileURL);
+            });
         } else {
-            alert('Please select a video file in MP4 format.');
+            alert('请上传MP4类型的文件格式');
         }
     });
     document.body.appendChild(fileInput);
     fileInput.click();
-    videoPlayer.addEventListener('loadedmetadata', extractFrames);
 }
 
-function extractFrames() {
-    const videoPlayer = document.getElementById('video-player');
+function extractFrames(videoPlayer) {
     const frameGallery = document.getElementById('image-container');
-    const frameRate = 1; // 设置帧率，这里是每秒1帧
     const numFrames = 6; // 设置要抽取的帧数
 
     const videoWidth = videoPlayer.videoWidth;
     const videoHeight = videoPlayer.videoHeight;
-
     const canvas = document.createElement('canvas');
     canvas.width = videoWidth;
     canvas.height = videoHeight;
-
     const context = canvas.getContext('2d');
-
-    for (let i = 0; i < numFrames; i++) {
-        const frameTime = i / frameRate;
-        videoPlayer.currentTime = frameTime;
+    let frameCount = 0;
+    let lastTime = 0;
+    let fps = 0;
+    function captureFrame(time) {
+        if (!lastTime) {
+            lastTime = time;
+        }
+        const delta = (time - lastTime) / 1000;
+        lastTime = time;
+        fps += 1 / delta;
         context.drawImage(videoPlayer, 0, 0, videoWidth, videoHeight);
         const image = canvas.toDataURL('image/png');
         const frameContainer = document.createElement('div');
@@ -48,5 +52,16 @@ function extractFrames() {
         frameImage.classList.add('frame-image');
         frameContainer.appendChild(frameImage);
         frameGallery.appendChild(frameContainer);
+        frameCount++;
+        if (frameCount < numFrames) {
+            requestAnimationFrame(captureFrame);
+        } else {
+            fps /= numFrames;
+            console.log('Average FPS:', fps);
+        }
     }
+    requestAnimationFrame(captureFrame);
+
+    const downloadButton = document.getElementById('container-download-button')
+    downloadButton.style.display = 'inline-block'
 }
